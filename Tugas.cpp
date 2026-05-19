e45e89396b0f53b034bafe6349bfe44a87e66b36
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-19 17:19:42 +0700

    fungsi untuk menampilkan rekening bank

--- Tugas.cpp
+++ Tugas.cpp
@@ -14,5 +14,11 @@ public:
 
     virtual void potongAdmin() = 0;
 
+    void tampilkanInfo() {
+        cout << "Nama  : " << nama << endl;
+        cout << "Saldo : Rp " << saldo << endl;
+        cout << "------------------------" << endl;
+    }
+
 
 };
